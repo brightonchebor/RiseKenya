a5a7a4b97dc420118f33a75ddd87f5f46dc533ae
Chebor <cheborbrighton805@gmailcom>
2024-10-08 20:14:38 +0300

    cn

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from .managers import UserManager
-# Create your models here.
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True, verbose_name=_("Email Address"))
@@ -15,7 +15,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
      
-    USERNAME_FIELDS = "email"
+    USERNAME_FIELD = "email"
 
     REQUIRED_FIELDS = ["first_name", "last_name"]
 
