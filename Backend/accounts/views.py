269c48743da92eaf2223576a736175da8726b615
Chebor <cheborbrighton805@gmailcom>
2024-10-12 06:49:52 +0300

    Otp verifcation view

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -4,6 +4,7 @@ from .serializers import UserRegisterSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from .utils import send_code_to_user
+from .models import OneTimePassword
 
 # Create your views here.
 class UserRegisterView(GenericAPIView):
@@ -25,3 +26,25 @@ class UserRegisterView(GenericAPIView):
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
+class VerifyUserEmail(GenericAPIView):
+
+    def post(self, request):
+        otpcode = request.data.get('otp')
+        try:
+            user_code_obj = OneTimePassword.objects.get(code=otpcode)
+            user = user_code_obj.user
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+                return Response({
+                    'message':'email account verified successfully'
+                }, status=status.HTTP_200_OK)
+            return Response({
+                'message':'code is invalid user already exist'
+            }, status=status.HTTP_204_NO_CONTENT)
+        
+        except OneTimePassword.DoesNotExist:
+            return Response({
+                'message':'passcode not provided'
+            }, status=status.HTTP_404_NOT_FOUND)
