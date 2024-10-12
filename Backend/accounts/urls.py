353ac284bedf4722c06c775188a3467289ab1e1f
Chebor <cheborbrighton805@gmailcom>
2024-10-12 06:58:57 +0300

    cn

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='register')
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('verify-email/', VerifyUserEmail.as_view(), name='verify'),
 ]
 
