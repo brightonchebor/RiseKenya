e6e59751eea980a05594db79551a64b5195132e3
Chebor <cheborbrighton805@gmailcom>
2024-10-14 15:26:37 +0300

    cn

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import User
+from .models import User, OneTimePassword
 
 # Register your models here.
-admin.site.register(User)
+# admin.site.register(User)
+
+admin.site.register(OneTimePassword)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'date_joined', 'last_login']
+
+
+
